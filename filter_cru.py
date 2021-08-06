Fixed y_mean scaling in ML optimisation loop plotting routine. Added summary plot of optimisation parameters as a function of low pass variance percentile.

filter_cru.py
@@ -1,45 +1,18 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-#------------------------------------------------------------------------------
-# PROGRAM: filter_cru.py
-#------------------------------------------------------------------------------
-#
-# translation of filter_cru.pro to python 3
-#
-# Version 0.1
-# 23 July, 2021
-# Michael Taylor
-# https://patternizer.github.io
-# patternizer AT gmail DOT com
-# michael DOT a DOT taylor AT uea DOT ac DOT uk
-#------------------------------------------------------------------------------
-
 import numpy as np
-import pandas as pd
-import xarray as xr
-import matplotlib.pyplot as plt
-
-#------------------------------------------------------------------------------
-# SETTINGS
-#------------------------------------------------------------------------------
-
-fontsize = 16
-baseline_start = 1981
-baseline_end = 2010
-#stationcode = '071560'
-stationcode = '744920'
-thalf = 300
-nan = False
-truncate = 0
-
-#------------------------------------------------------------------------------
-# METHODS
-#------------------------------------------------------------------------------
 
 def cru_filter(thalf, tsin, nan, truncate):
     
-    ''' -----------------------------------------------------------------------------
+    ''' 
+    -----------------------------------------------------------------------------
+    translation of filter_cru.pro from IDL --> python 3.8+
+    -----------------------------------------------------------------------------
+    Version 0.1
+    29 July, 2021	
+    Michael Taylor
+    https://patternizer.github.io
+    patternizer AT gmail DOT com
+    michael DOT a DOT taylor AT uea DOT ac DOT uk
+    #------------------------------------------------------------------------------
     Uses the CRU Gaussian weighted filter to separate the low and high frequency
     components of a timeseries (or a set of time series of equal length).
 
@@ -47,8 +20,6 @@ def cru_filter(thalf, tsin, nan, truncate):
     tsin           : input timeseries (1D or 2D, if the latter then assume order is [SERIES,TIME]
     nan            : If this flag is set, missing values in tsin are replaced by the timeseries local mean before filtering, and then 
                      re-replaced by the missing code afterwards (and in the high and low frequency series too)
-    tslow,tshigh   : low and high frequency components
-    weight         : optionally return weights used
     truncate       : 0=pad with data and filter right to ends,
                    : 1=truncate filtered series to stop (thalf-1)/4 from end
                    : 2=truncate filtered series to stop (thalf-1)/2 from end
@@ -57,7 +28,13 @@ def cru_filter(thalf, tsin, nan, truncate):
                      by nan=0 (though with less control).  If a series has missing data past the start or end of the series already,
                      then TRUNCATE will not truncate the actual data values, but only the values from the ends of the complete series
                      including missing code.
-    ----------------------------------------------------------------------------- '''
+                     
+    tslow,tshigh   : low and high frequency components
+    weight         : optionally return weights used
+    ----------------------------------------------------------------------------- 
+    CALL SYNTAX: tslow, tshigh, weight = cru_filter(thalf, tsin, nan, truncate)                                 
+    ----------------------------------------------------------------------------- 
+    '''
 
     # DEFINE: arrays
 
@@ -81,7 +58,6 @@ def cru_filter(thalf, tsin, nan, truncate):
         nw = nw + 1
     if nw < 7:
         nw = 7
-    print('nw',nw)
     weight = np.zeros(nw)
 
     # COMPUTE: weights
@@ -195,61 +171,4 @@ def cru_filter(thalf, tsin, nan, truncate):
                 tslow[:,nt-nend:] = np.nan
 
     return tslow, tshigh, weight
-
 #------------------------------------------------------------------------------
-# LOAD: a station anomaly timeseries
-#------------------------------------------------------------------------------
-
-df_temp = pd.read_pickle('DATA/df_temp.pkl', compression='bz2')    
-da = df_temp[df_temp['stationcode'] == stationcode]
-stationname = da['stationname'].iloc[0]
-da = da[da.year >= 1678].reset_index(drop=True)
-db = da.copy()
-for j in range(1,13):
-    baseline = np.nanmean(np.array(da[(da['year'] >= 1981) & (da['year'] <= 2010)].iloc[:,j]).ravel())
-    db.loc[da.index.tolist(), str(j)] = da[str(j)]-baseline                        
-ts = np.array(db.groupby('year').mean().iloc[:,0:12]).ravel()
-t = pd.date_range(start=str(db.year.iloc[0]), periods=len(ts), freq='MS')
-df = pd.DataFrame({'Tg':ts}, index=t)   
-
-# RESAMPLE: to yearly
-
-df_xr = df.to_xarray()    
-df_xr_resampled = df_xr.Tg.resample(index='AS').mean().to_dataset()    
-
-tsin = df_xr_resampled.Tg.values
-t = df_xr_resampled.index.values
-
-#------------------------------------------------------------------------------
-# CALL: cru_filter
-#------------------------------------------------------------------------------
-
-tslow, tshigh, weight = cru_filter(thalf, tsin, nan, truncate)
-
-#------------------------------------------------------------------------------
-# PLOT: ts and tslow and tshigh components
-#------------------------------------------------------------------------------
-
-figstr = stationcode + '-' + 'gaussian-filter' + '.png'
-titlestr = 'GloSAT.p03: ' + stationname + ' (' + stationcode + '): CRU Gaussian filter (thalf=' + str(thalf) + ', nw=' + str(len(weight)) + ')'
-               
-fig, ax = plt.subplots(figsize=(15,10))    
-plt.fill_between(t, 0, tsin, ls='-', lw=3, color='black', alpha=0.1, zorder=1, label=r'tsin')
-plt.plot(t, tslow.T, ls='-', lw=3, color='teal', alpha=1, zorder=1, label=r'tslow')
-plt.plot(t, tshigh.T, ls='--', lw=1, color='teal', alpha=1, zorder=1, label=r'tshigh')
-plt.axhline(y=np.nanmean(tshigh.T), ls='--', lw=1, color='black', alpha=1, zorder=1, label=r'$\mu(tshigh)$')
-plt.xlabel('Year', fontsize=fontsize)
-#ax.set_xlim(pd.to_datetime('1720-01-01'),pd.to_datetime('2021-01-01'))
-plt.ylabel(r'2m Temperature anomaly (from ' + str(baseline_start) + '-' + str(baseline_end) + r'), $^{\circ}$C', fontsize=fontsize)
-plt.title(titlestr, fontsize=fontsize)
-plt.tick_params(labelsize=fontsize)    
-plt.legend(loc='lower right', ncol=1, markerscale=1, facecolor='lightgrey', framealpha=0.5, fontsize=fontsize)    
-fig.tight_layout()
-plt.savefig(figstr, dpi=300)
-plt.close('all')
-
-#------------------------------------------------------------------------------
-print('** END')
-
-
-

ts-dft-filter-lowpass-optimiser.py
@@ -4,8 +4,8 @@
 #------------------------------------------------------------------------------
 # PROGRAM: ts-fft-filter-lowpass-optimiser.py
 #------------------------------------------------------------------------------
-# Version 0.2
-# 28 July, 2021
+# Version 0.3
+# 6 August, 2021
 # Michael Taylor
 # https://patternizer.github.io
 # patternizer AT gmail DOT com
@@ -24,10 +24,12 @@ import seaborn as sns; sns.set()
 stationcode = '037401'
 
 fontsize = 16
-make_plot_loop = False                                           # True --> loop over pctl [0,100,0.1] to generate LUT
+make_plot_loop = False                                          # True --> loop over pctl [0,100,0.1] to generate LUT
 use_filter = True                                               # True --> Hamming window, False --> cut-off (no window)
 show_pandas = True                                              # Overlay equi-window Pandas smoother
-   
+show_gaussian = True                                            # Overlay CRU Gaussian filter   
+pctl_stepsize = 0.01
+
 #-----------------------------------------------------------------------------
 # METHODS
 #-----------------------------------------------------------------------------
@@ -94,7 +96,7 @@ def dft_filter(y):
     vec_zvarhi = []
     vec_y_hi_mean = []
         
-    for pctl in np.arange(0,100,0.1):
+    for pctl in np.arange(0,100,pctl_stepsize):
                                
         zpeaks = zvarpc[ zvarpc > ( np.percentile(zvarpc, pctl) ) ]     # high variance peaks > p(pctl)
         zpeaks_idx = np.argsort( zpeaks )                               # peak indices
@@ -165,14 +167,14 @@ def dft_filter(y):
                             
             fig, ax = plt.subplots(figsize=(15,10))
             plt.subplot(211)
-            plt.plot(t, y, ls='-', lw=1, color='blue', alpha=0.5, label='signal: yearly')                             
+            plt.plot(t, y + y_mean, ls='-', lw=1, color='blue', alpha=0.5, label='signal: yearly')                             
             if show_pandas == True:
                 if fc < 1e-16:
                     T = N
                 else:
                     T = int(1/fc)            
-                plt.plot(t, pd.Series(y).rolling(T,center=True).mean(), ls='-', lw=3, color='red', alpha=1, label='Pandas rolling(' + str(T) + ')')            
-            plt.plot(t, y_filtered_lo, ls='-', lw=3, color='blue', label=r'DFT low pass ($\nu$=' + str(np.round(zvarlo*100.0,2)) + '%)' + ': T=' + str(T) + ', fc=' + str(np.round(fc,2)) + r', P$\geq$p(' + str(np.round(pctl,1)) + ')')   
+                plt.plot(t, pd.Series(y + y_mean).rolling(T,center=True).mean(), ls='-', lw=3, color='red', alpha=1, label='Pandas rolling(' + str(T) + ')')            
+            plt.plot(t, y_filtered_lo + y_mean, ls='-', lw=3, color='blue', label=r'DFT low pass ($\nu$=' + str(np.round(zvarlo*100.0,2)) + '%)' + ': w=' + str(T) + ', fc=' + str(np.round(fc,2)) + r', P$\geq$p(' + str(np.round(pctl,1)) + ')')   
             plt.ylim(-3,2)
             plt.tick_params(labelsize=fontsize)    
             plt.ylabel(r'T(2m) anomaly (from 1981-2010), $^{\circ}$C', fontsize=fontsize)
@@ -193,7 +195,7 @@ def dft_filter(y):
 
     df = pd.DataFrame({'pctl':vec_pctl, 'fc':vec_fc, 'zvarlo':vec_zvarlo, 'zvarhi':vec_zvarhi, 'y_hi_mean':vec_y_hi_mean})
     df['period'] = 1.0/df['fc']
-    df.to_csv('ml_optimisation.csv')                       
+#    df.to_csv('ml_optimisation.csv')                       
                         
     return y_filtered_lo + y_mean, y_filtered_hi, zvarlo, zvarhi, pctl, fc
 
@@ -239,18 +241,33 @@ if __name__ == "__main__":
                             
     fig, ax = plt.subplots(figsize=(15,10))
     plt.subplot(211)
-    plt.plot(df.pctl, df.fc, ls='-', lw=3, color='purple', alpha=1, label='fc')                             
-    plt.plot(df.pctl, df.zvarlo, ls='-', lw=3, color='red', label=r'DFT low pass ($\nu$)')   
-    plt.plot(df.pctl, df.zvarhi, ls='-', lw=3, color='blue', label=r'DFT high pass ($\nu$)')   
+    plt.fill_between(df.pctl, 0, df.zvarlo, ls='-', lw=3, color='red', alpha=0.5, label=r'DFT low pass variance ($\nu$) fraction')   
+    plt.fill_between(df.pctl, 0, df.zvarhi, ls='-', lw=3, color='blue', alpha=0.5, label=r'DFT high pass variance ($\nu$) fraction')   
+    plt.axhline(y=0.5, ls='--', lw=1, color='black')
+    plt.axvline(x=50, ls='--', lw=1, color='black')    
+    plt.text(x=50+0.5, y=0.0+0.02, s='P=p(50)', fontsize=fontsize)
+    plt.text(x=0+0.5, y=0.5+0.02, s=r'Nyquist frequency: $f_{s}/2=0.5 Hz$', fontsize=fontsize)        
+    plt.plot(df.pctl, df.fc, ls='--', lw=3, color='teal', alpha=1, label='frequency cut-off ($f_c$)')                             
+    plt.plot(df.pctl, 1./(df.fc*len(y)), ls='--', lw=3, color='darkorange', alpha=1, label='Smoothing (w/N)')                             
+    plt.xlim(0,100)
+    plt.ylim(0,1)
     plt.tick_params(labelsize=fontsize)    
-    plt.xlabel('Percentile', fontsize=fontsize)
-    plt.ylabel('Magnitude', fontsize=fontsize)
-    plt.legend(loc='upper center', ncol=3, markerscale=1, facecolor='lightgrey', framealpha=0.5, fontsize=fontsize)   
+#   plt.xlabel('Percentile (P), %', fontsize=fontsize)
+#   plt.ylabel('Magnitude', fontsize=fontsize)
+    plt.legend(loc='upper center', ncol=1, markerscale=1, facecolor='lightgrey', framealpha=0.5, fontsize=fontsize)   
+
     plt.subplot(212)
-    plt.plot(df.pctl, df.y_hi_mean, ls='-', lw=3, color='teal', alpha=1, label=r'DFT high pass ($\nu$) mean')
+
+    ymax = np.nanmax( np.abs( df.y_hi_mean ) )
+
+    plt.plot(df.pctl, df.y_hi_mean, ls='-', lw=3, color='teal', alpha=1, label=r'DFT high pass signal mean ($\mu$)')
+    plt.axhline(y=0.0, ls='--', lw=1, color='black')    
+    plt.text(x=0+0.5, y=0.0+ymax/20, s=r'$\mu$=0.0', fontsize=fontsize)        
+    plt.xlim(0,100)
+    plt.ylim(-0.01,0.01)
     plt.tick_params(labelsize=fontsize)    
-    plt.xlabel('Percentile', fontsize=fontsize)
-    plt.ylabel('Magnitude', fontsize=fontsize)
+    plt.xlabel('Percentile (P), %', fontsize=fontsize)
+#   plt.ylabel('Magnitude', fontsize=fontsize)
     plt.legend(loc='upper center', ncol=3, markerscale=1, facecolor='lightgrey', framealpha=0.5, fontsize=fontsize)   
 #   plt.suptitle(stationname + ' (' + stationcode + ')')
     plt.suptitle('HadCET (' + stationcode + ')')